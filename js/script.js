d372d777655ae924290a95add52cc70efcee5ad9
castabit <noomor36@gmail.com>
2024-12-16 23:39:58 -0500

    added index page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
-const numberList = document.getElementById('number-list');
+const navList = document.getElementById("nav-list");
 
-for (let i = 1; i <= 10; i++)
-{
-    const listItem = document.createElement('li');
-    listItem.textContent = i;
-    numberList.appendChild(listItem);
+const navBar = {
+    Home: "/index.html",
+    About: "/pages/about.html",
+    Rankings: "/pages/rankings.html",
+}; 
+
+for (const [key,value] of Object.entries(navBar)) {
+    const listItem = document.createElement("li");
+
+    const anchor = document.createElement("a");
+    anchor.textContent = key;
+    anchor.setAttribute("href",value);
+    listItem.appendChild(anchor);
+    navList.appendChild(listItem);
 }
+
+// const headers = ["Home","About","Rankings"];
+
+// headers.forEach(populateHeader);
+
+// function populateHeader(item)
+// {
+//     const listItem = document.createElement("li");
+//     listItem.textContent = item;
+//     navList.appendChild(listItem);
+// }
