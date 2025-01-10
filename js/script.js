7e6b031cdedf02ff4b76b8d3139bc19ff0a9e1bb
castabit <noomor36@gmail.com>
2025-01-09 23:47:42 -0500

    added js to commit

--- js/script.js
+++ js/script.js
@@ -16,3 +16,24 @@ for (const [key,value] of Object.entries(navBar)) {
     listItem.appendChild(anchor);
     navList.appendChild(listItem);
 }
+
+const footer = document.querySelector("footer");
+for (i = 0; i < 3; i++)
+{
+    const div = document.createElement("div");
+    footer.appendChild(div);
+    for (j = 0; j < 3; j++)
+    {
+        const p = document.createElement("p");
+        const a = document.createElement("a");
+        a.setAttribute("href","#top");
+        a.textContent = "placeholder";
+        p.appendChild(a);
+        div.appendChild(p);
+    }
+}
+// footer.textContent = "Footer Content";
+
+// const h2 = document.createElement("h2");
+// h2.textContent = "Footer Content";
+// footer.appendChild(h2);
